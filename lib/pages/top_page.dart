5024fbaa853a70d3853381340ce5d78d7d41d40a
Ryunosuke Aoki <ryunosuke1114@gmail.com>
2022-03-20 11:43:48 +0900

    fix fontsize and margin

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -12,7 +12,7 @@ class TopPage extends StatelessWidget {
       child: Scaffold(
         body: Stack(children: [
           Container(
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               image: DecorationImage(
                 image: AssetImage(
                   'images/qiita.png',
@@ -21,66 +21,65 @@ class TopPage extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            child: Center(
-              child: Column(
-                children: [
-                  Expanded(flex: 3, child: Container()),
-                  Text(
-                    'Qiita Feed App',
-                    style: GoogleFonts.pacifico(
-                      fontSize: 40.0,
-                      color: Colors.white,
-                    ),
+          Center(
+            child: Column(
+              children: [
+                Expanded(flex: 3, child: Container()), //端末によって見え方に違いが現れないように
+                Text(
+                  'Qiita Feed App',
+                  style: GoogleFonts.pacifico(
+                    fontSize: 36.0,
+                    color: Colors.white,
                   ),
-                  SizedBox(height: 10.0),
-                  Text(
-                    '-PlayGround-',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
-                      color: Colors.white,
-                    ),
+                ),
+                const SizedBox(height: 10.0),
+                const Text(
+                  '-PlayGround-',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14.0,
+                    color: Colors.white,
                   ),
-                  Expanded(flex: 4, child: Container()),
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 24),
-                    height: 50,
-                    width: MediaQuery.of(context).size.width,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        'ログイン',
-                        style: TextStyle(
-                          fontSize: 17.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        primary: HexColor('#45970e'),
-                        shape: const StadiumBorder(),
+                ),
+                Expanded(flex: 4, child: Container()),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 24),
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'ログイン',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
+                    style: ElevatedButton.styleFrom(
+                      primary: HexColor('#468300'),
+                      shape: const StadiumBorder(),
+                    ),
                   ),
-                  SizedBox(height: 20),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 60),
-                    height: 50,
-                    width: MediaQuery.of(context).size.width,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'ログインせずに利用する',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17.0,
-                        ),
+                ),
+                const SizedBox(height: 34.0),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 24),
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'ログインせずに利用する',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14.0,
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+                const SizedBox(height: 81),
+              ],
             ),
           ),
         ]),
