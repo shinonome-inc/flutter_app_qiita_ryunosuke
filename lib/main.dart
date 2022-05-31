953ee4ec56167cc261d6314fea15ab7e957292d7
Ryunosuke Aoki <ryunosuke1114@gmail.com>
2022-05-31 22:21:42 +0900

    add not login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_qiita/pages/top_page.dart';
 import 'bottom_navigationbar/bottom_navigation.dart';
 
 void main() => runApp(
       const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: BottomNavigation(),
+        home: TopPage(),
       ),
     );
