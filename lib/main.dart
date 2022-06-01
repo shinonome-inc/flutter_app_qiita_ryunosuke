74952316fd4008dd7a9876078c8317571c0e0297
Ryunosuke Aoki <ryunosuke1114@gmail.com>
2022-06-01 20:33:38 +0900

    remove unnecessary import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_qiita/pages/top_page.dart';
-import 'bottom_navigationbar/bottom_navigation.dart';
 
 void main() => runApp(
       const MaterialApp(
