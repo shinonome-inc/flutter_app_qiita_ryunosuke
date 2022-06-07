4206264716cc14a55b7c3ddf62bde9aedfa00363
Ryunosuke Aoki <ryunosuke1114@gmail.com>
2022-06-07 16:09:45 +0900

    add Blur

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'package:flutter_app_qiita/service/qiita_client.dart';
@@ -41,105 +43,116 @@ class _TopPageState extends State<TopPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(
-                'images/qiita.png',
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'images/qiita.png',
+                ),
+                fit: BoxFit.cover,
               ),
-              fit: BoxFit.cover,
             ),
           ),
-        ),
-        Positioned.fill(
-          child: Container(
-            color: Colors.black.withOpacity(0.2),
+          Positioned.fill(
+            child: Container(
+              color: Colors.black.withOpacity(0.2),
+            ),
           ),
-        ),
-        Center(
-          child: Column(
-            children: [
-              Expanded(flex: 3, child: Container()), //端末によって見え方に違いが現れないように
-              Text(
-                'Qiita Feed App',
-                style: GoogleFonts.pacifico(
-                  fontSize: 36.0,
-                  color: Colors.white,
+          Center(
+            child: Column(
+              children: [
+                Expanded(flex: 3, child: Container()), //端末によって見え方に違いが現れないように
+                Text(
+                  'Qiita Feed App',
+                  style: GoogleFonts.pacifico(
+                    fontSize: 36.0,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10.0),
-              const Text(
-                '-PlayGround-',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14.0,
-                  color: Colors.white,
+                const SizedBox(height: 10.0),
+                const Text(
+                  '-PlayGround-',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14.0,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              Expanded(flex: 4, child: Container()),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 24),
-                height: 50,
-                width: MediaQuery.of(context).size.width,
-                child: ElevatedButton(
-                  onPressed: () {
-                    showModalBottomSheet(
-                      context: context,
-                      enableDrag: true,
-                      isScrollControlled: true,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.vertical(
-                          top: Radius.circular(10.0),
+                Expanded(flex: 4, child: Container()),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 24),
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                        context: context,
+                        enableDrag: true,
+                        isScrollControlled: true,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(10.0),
+                          ),
                         ),
+                        builder: (context) {
+                          return const LoginPage();
+                        },
+                      );
+                    },
+                    child: const Text(
+                      'ログイン',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
                       ),
-                      builder: (context) {
-                        return const LoginPage();
-                      },
-                    );
-                  },
-                  child: const Text(
-                    'ログイン',
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.bold,
                     ),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.lightGreen[900],
-                    shape: const StadiumBorder(),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 34.0),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 24),
-                height: 50,
-                width: MediaQuery.of(context).size.width,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const BottomNavigation(),
-                      ),
-                    );
-                  },
-                  child: const Text(
-                    'ログインせずに利用する',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14.0,
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.lightGreen[900],
+                      shape: const StadiumBorder(),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 81),
-            ],
+                const SizedBox(height: 34.0),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 24),
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const BottomNavigation(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'ログインせずに利用する',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 81),
+              ],
+            ),
           ),
-        ),
-      ]),
+          if (isLoading)
+            BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: isLoading ? 8 : 0,
+                sigmaY: isLoading ? 8 : 0,
+              ),
+              child: Container(color: const Color(0x00000000).withOpacity(0)),
+            ),
+          if (isLoading) const Center(child: CircularProgressIndicator()),
+        ],
+      ),
     );
   }
 }
