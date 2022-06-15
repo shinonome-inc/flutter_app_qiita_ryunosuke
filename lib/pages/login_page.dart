5ef0e2d67047f8a89fc1155f8520b3d63583419a
Ryunosuke Aoki <ryunosuke1114@gmail.com>
2022-06-15 15:03:30 +0900

    add keybord height

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -20,19 +20,20 @@ class _LoginPageState extends State<LoginPage> {
   WebViewPlusController? _controller;
 
   late final String uri;
+  double webViewHeight = 1;
 
   Future<void> onPageFinished(BuildContext context, String url) async {
     final uri = Uri.parse(url);
     if (uri.queryParameters['code'] != null) {
       onAuthorizeCallbackIsCalled(uri);
     }
-    await pageLoading(context, url);
+    await pageLoading();
   }
 
-  Future<void> pageLoading(BuildContext context, String url) async {
-    _controller?.getHeight().then((double height) {
+  Future<void> pageLoading() async {
+    await _controller?.getHeight().then((double height) {
       setState(() {
-        height = height;
+        webViewHeight = height;
       });
     });
   }
@@ -58,46 +59,38 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
-        reverse: true,
-        child: Padding(
-          padding: EdgeInsets.only(bottom: keyboardHeight),
-          child: Column(
-            children: [
-              Container(
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(10.0)),
-                ),
-                height: 59.0,
-                child: Center(
-                  child: Text(
-                    "Qiita Auth",
-                    style: GoogleFonts.pacifico(
-                      fontSize: 17,
-                      color: Colors.black,
-                    ),
-                  ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(10.0)),
+            ),
+            height: 59.0,
+            child: Center(
+              child: Text(
+                "Qiita Auth",
+                style: GoogleFonts.pacifico(
+                  fontSize: 17,
+                  color: Colors.black,
                 ),
               ),
-              SizedBox(
-                height: size.height,
-                child: WebViewPlus(
-                  initialUrl: QiitaClient.createAuthorizeUrl(),
-                  javascriptMode: JavascriptMode.unrestricted,
-                  onPageFinished: (String url) => onPageFinished(context, url),
-                  onWebViewCreated: (controller) {
-                    _controller = controller;
-                  },
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+          SizedBox(
+            height: webViewHeight + keyboardHeight,
+            child: WebViewPlus(
+              initialUrl: QiitaClient.createAuthorizeUrl(),
+              javascriptMode: JavascriptMode.unrestricted,
+              onPageFinished: (String url) => onPageFinished(context, url),
+              onWebViewCreated: (controller) {
+                _controller = controller;
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
