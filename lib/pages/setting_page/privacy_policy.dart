c215c1c90be040af8e17a5ef4a3bb3af855b6616
Ryunosuke Aoki <ryunosuke1114@gmail.com>
2022-07-09 13:20:36 +0900

    feat : dev plivacy policy page UI

---
+++ lib/pages/setting_page/privacy_policy.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PrivacyPolicy extends StatelessWidget {
+  const PrivacyPolicy({Key? key}) : super(key: key);
+
+  final String text = """
+プライバシーポリシー
+
+
+情報の収集と使用
+第三者に個人を特定できる情報を提供することはありません。
+より良い体験のために、私たちのサービスを使用している間、特定の個人情報を提供するようにあなたに要求する場合があります。
+アプリは、あなたを識別するために使用される情報を収集する可能性のあるサードパーティのサービスを使用します。
+  
+アプリで使用されるサードパーティのサービスプロバイダー
+  
+- ログデータ
+  
+サービスを使用するたびに、アプリでエラーが発生した場合、ログデータと呼ばれる電話でデータと情報を（サードパーティ製品を通じて）収集することをお知らせします。このログデータには、デバイスのインターネットプロトコル（「IP」）アドレス、デバイス名、オペレーティングシステムのバージョン、サービスを利用する際のアプリの構成、サービスの使用日時、その他の統計などの情報が含まれる場合があります。
+  
+Qiita Feed Appではアプリの利便性向上を目的にして、個人を特定できないよう匿名化したデータを用いてアクセス解析を行なっています。
+例えばアプリのクラッシュ情報を匿名で送信し、バグの修正のために利用しています。
+  
+またご利用端末のOSやアプリバージョンの使用率などを解析しアプリの改善に役立てています。
+  
+このプライバシーポリシーの変更
+  
+プライバシーポリシーを随時更新する場合があります。したがって、変更がある場合はこのページを定期的に確認することをお勧めします。このページに新しいプライバシーポリシーを掲載して、変更をお知らせします。これらの変更は、このページに投稿された直後に有効になります。
+  
+お問い合わせ
+  
+プライバシーポリシーについてご質問やご提案がありましたら、@gmail.comまでお気軽にお問い合わせください。
+
+
+""";
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            color: Color(0XF9F9F9F0),
+            borderRadius: BorderRadius.vertical(top: Radius.circular(10.0)),
+          ),
+          height: 59.0,
+          child: const Center(
+            child: Text(
+              "プライバシーポリシー",
+              style: TextStyle(
+                fontSize: 17,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+        Container(
+          color: Colors.white,
+          height: 18,
+        ),
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.only(left: 16, right: 16),
+            color: Colors.white,
+            child: SingleChildScrollView(
+              child: Text(
+                text,
+                style: const TextStyle(
+                  fontSize: 14,
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/setting_page/setting_page.dart
+++ lib/pages/setting_page/setting_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_qiita/components/appbar_design.dart';
 import 'package:flutter_app_qiita/components/setting_page_item_component.dart';
+import 'package:flutter_app_qiita/pages/setting_page/privacy_policy.dart';
 import 'package:flutter_app_qiita/service/qiita_client.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
@@ -57,19 +58,36 @@ class _SettingPageState extends State<SettingPage> {
               style: TextStyle(color: Color(0xFF828282), fontSize: 12.0),
             ),
           ),
-          const SettingPageItemComponent(
+          SettingPageItemComponent(
             text: 'プライバシーポリシー',
-            item: Icon(
+            item: const Icon(
               Icons.arrow_forward_ios_outlined,
               color: Colors.grey,
             ),
+            onTap: () {
+              showModalBottomSheet(
+                  enableDrag: true,
+                  isScrollControlled: true,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(10.0),
+                    ),
+                  ),
+                  context: context,
+                  builder: (context) {
+                    return SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.95,
+                        child: const PrivacyPolicy());
+                  });
+            },
           ),
-          const SettingPageItemComponent(
+          SettingPageItemComponent(
             text: '利用規約',
-            item: Icon(
+            item: const Icon(
               Icons.arrow_forward_ios_outlined,
               color: Colors.grey,
             ),
+            onTap: () {},
           ),
           SettingPageItemComponent(
             text: 'アプリバージョン',
@@ -77,6 +95,7 @@ class _SettingPageState extends State<SettingPage> {
               'v' + _packageInfo.version,
               style: const TextStyle(fontSize: 14.0, color: Colors.black),
             ),
+            onTap: () {},
           ),
           const SizedBox(
             height: 36.0,
@@ -92,6 +111,7 @@ class _SettingPageState extends State<SettingPage> {
             SettingPageItemComponent(
               text: 'ログアウトする',
               item: Container(),
+              onTap: () {},
             )
         ],
       ),
